7d14931c219ad6e673efa6570c9fdc96d7114aee
Jish2612 <167243813+Jish2612@users.noreply.github.com>
2026-02-16 23:33:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
+	e.preventDefault();
+
+    const fname = document.querySelector('input[name="fname"]').value.trim();
+    const lname = document.querySelector('input[name="lname"]').value.trim();
+
+    alert(`${fname} ${lname}`.trim());
 
 }
